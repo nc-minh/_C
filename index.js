e08a7cd4a7f9f0ffc6d34b733830026b49dbb8f2
nc-minh <nguyencongminh.official@gmail.com>
2022-05-08 12:20:46 +0700

    update :)))))) so frustrated

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const app = require('express')();
+const express = require("express");
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require("path");
 const port = process.env.PORT || 2512;
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
 });
+
+//config public resources
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+//route init
+const route = require('./src/routes/index');
+route(app)
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const homeRouter = require("./home.route");
+
+function route(app) {
+  app.use("/", homeRouter);
+}
+
+module.exports = route;
